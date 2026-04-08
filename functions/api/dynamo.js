f3556a176a53a0990007f6b74218cd78ced83445
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-04-08 10:17:35 +0200

    Fix backend url path for hello

--- functions/api/dynamo.js
+++ functions/api/dynamo.js
@@ -2,17 +2,17 @@ import { createSignedFetcher } from 'aws-sigv4-fetch';
 
 export const onRequest = async (context) => {
     console.log({
-    url: context.request.url,
-    method: context.request.method,
-    headers: Object.fromEntries(context.request.headers)
+        url: context.request.url,
+        method: context.request.method,
+        headers: Object.fromEntries(context.request.headers)
     });
 
     const signedFetch = createSignedFetcher({
         service: 'lambda',
         region: 'eu-central-1',
         credentials: {
-        accessKeyId: context.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: context.env.AWS_ACCESS_KEY_SECRET,
+            accessKeyId: context.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: context.env.AWS_ACCESS_KEY_SECRET,
         }
     });
 
--- functions/api/hello.js
+++ functions/api/hello.js
@@ -1,20 +1,24 @@
 import { createSignedFetcher } from 'aws-sigv4-fetch';
 
 export const onRequest = async (context) => {
-    const options = {
+    const signedFetch = createSignedFetcher({
         service: 'lambda',
         region: 'eu-central-1',
         credentials: {
             accessKeyId: context.env.AWS_ACCESS_KEY_ID,
             secretAccessKey: context.env.AWS_ACCESS_KEY_SECRET,
         }
-        };
-    const signedFetch = createSignedFetcher(options);
-    const response = await signedFetch(context.env.BACKEND_URL + '/hello',
-        {
-            method: 'GET',
-        },
-        options
-        );
-  return response;
+    });
+
+    const incoming = new URL(context.request.url);
+    const backendUrl = new URL(incoming.pathname, context.env.BACKEND_URL);
+    console.log("backendUrl:", backendUrl);
+
+    const response = await signedFetch(backendUrl, {
+        method: context.request.method,
+    });
+
+    console.log("status:", response.status);
+
+    return response;
 };
